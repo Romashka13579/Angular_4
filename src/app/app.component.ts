35750998800b5e54c5070369544de4d751ac0aa7
Romashka <rroma13579@gmail.com>
2023-01-16 14:35:56 +0100

    A heroes tour

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'add-one-button',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular_4';
+  
+  count = 0;
+}
