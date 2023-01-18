da9fbb8d97ca14c870e48755b8f3cf459d761d86
Romashka <rroma13579@gmail.com>
2023-01-18 22:18:14 +0100

    hero class

---
+++ src/app/hero.ts
@@ -0,0 +1,13 @@
+export class HeroInfo {
+
+    constructor(
+      public id: number,
+      public name: string,
+      public superName: string,
+      public superPower: string,
+      public element: string
+    ) {  }
+  
+  }
+
+  const firstHero =  new HeroInfo(42, 'Roma', 'Romashka', 'Create flowers', 'nature');
