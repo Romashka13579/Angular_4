d6d38f5705ff1336a7201dfdf92e66b21116dc2b
Romashka <rroma13579@gmail.com>
2023-01-22 23:20:52 +0100

    hero-details is a seperated object

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
+    HeroDetailsComponent,
   ],
   imports: [
     BrowserModule,
