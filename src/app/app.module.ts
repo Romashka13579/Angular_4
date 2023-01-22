1c3e4a43723c53f51d4d638883988633a1b9d7be
Romashka <rroma13579@gmail.com>
2023-01-22 19:43:19 +0100

    back to start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroFormComponent } from './hero-form/hero-form.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroFormComponent,
+    HeroesComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,3 +1,8 @@
+export interface Hero {
+  id: number;
+  name: string;
+}
+
 export class HeroInfo {
 
     constructor(
