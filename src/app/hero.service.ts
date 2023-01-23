c093a655449204b6e57921b28fc9a8ecdba2abf1
Romashka <rroma13579@gmail.com>
2023-01-23 23:01:42 +0100

    services

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { listOfHeroes } from './mock-heroes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+
+  getHeroes(): Hero[]{
+    return listOfHeroes;
+  }
 
   constructor() { }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { listOfHeroes } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  heroes = listOfHeroes;
+export class HeroesComponent implements OnInit{
+
+  constructor(private heroService:HeroService){}
+
+  getHeroes(): void{
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  heroes: Hero[] = [];
 
   selectedHero?:Hero;
 
   Select(hero:Hero): void{
     this.selectedHero = hero;
+  }
+
+  ngOnInit(){
+    this.getHeroes();
   }
 }
