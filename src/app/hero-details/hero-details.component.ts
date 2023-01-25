ba9ca91db808feda44f1ed7ac1c8dada265836ba
Romashka <rroma13579@gmail.com>
2023-01-25 23:50:41 +0100

    problem

--- src/app/hero-details/hero-details.component.ts
+++ src/app/hero-details/hero-details.component.ts
@@ -1,11 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-details',
   templateUrl: './hero-details.component.html',
   styleUrls: ['./hero-details.component.css']
 })
-export class HeroDetailsComponent {
-  @Input() hero?: Hero;
+export class HeroDetailsComponent implements OnInit {
+  hero: Hero | undefined;
+
+  constructor(
+    private route:ActivatedRoute, 
+    private heroService:HeroService, 
+    location:Location
+  ){}
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  ngOnInit(): void {
+    this.getHero();
+  }
 }
